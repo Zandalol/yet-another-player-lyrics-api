1c9d1d50dff23ebb64ccc6a3ecc84150e2f62395
Zandalol <Leptus365@gmail.com>
2022-12-02 22:12:21 +0700

    3

--- index.js
+++ index.js
@@ -2,10 +2,9 @@ require('dotenv').config()
 const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
-const chromium = require('chrome-aws-lambda');
 
 
-const getURL = async (title) => {
+const getURL = async (title, artist) => {
 	try {
 		const response = await axios({
 			method: 'GET',
@@ -19,6 +18,11 @@ const getURL = async (title) => {
 				'q': title
 			}
 		});
+		if (artist === undefined) {
+			for (const hit of response.data.response.hits) {
+				console.log(hit.result.featured_artists);
+			}
+		}
 		return response.data.response.hits[0].result.url;
 	} catch (error) {
 		console.error(error);
@@ -26,42 +30,12 @@ const getURL = async (title) => {
 };
 
 
-const scrapeLyrics = async (url) => {
-	const browser = await chromium.puppeteer.launch({
-		args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
-		defaultViewport: chromium.defaultViewport,
-		executablePath: await chromium.executablePath,
-		headless: true,
-		ignoreHTTPSErrors: true,
-	});
-
-	const page = await browser.newPage();
-	await page.setRequestInterception(true);
-	page.on('request', (req) => {
-		if (req.resourceType() == 'font' || req.resourceType() == 'image') {
-			req.abort();
-		}
-		else {
-			req.continue();
-		}
-	});
-	await page.goto(url);
-
-	const selector = `div[data-lyrics-container="true"]`
-	await page.waitForSelector(selector);
-	const lyrics = await page.$$eval(selector, arrayOfDivs => arrayOfDivs.map(div => div.innerText));
-
-	await browser.close();
-
-	return lyrics;
-};
-
-
 const app = express();
 app.use(cors());
 
 app.get("/", async (req, res) => {
-	res.send(await scrapeLyrics(await getURL('дао')));
+	if (req.query.title) res.send(await getURL(req.query.title))
+	else res.send(null);
 });
 
 const port = process.env.PORT || 5000;
