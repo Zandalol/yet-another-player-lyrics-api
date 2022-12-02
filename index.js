1090a1091ec4acb1f683dda763144415f29ff655
Zandalol <Leptus365@gmail.com>
2022-12-02 22:19:32 +0700

    Revert "3"
    This reverts commit 1c9d1d50dff23ebb64ccc6a3ecc84150e2f62395.

--- index.js
+++ index.js
@@ -2,9 +2,10 @@ require('dotenv').config()
 const axios = require('axios');
 const express = require('express');
 const cors = require('cors');
+const chromium = require('chrome-aws-lambda');
 
 
-const getURL = async (title, artist) => {
+const getURL = async (title) => {
 	try {
 		const response = await axios({
 			method: 'GET',
@@ -18,11 +19,6 @@ const getURL = async (title, artist) => {
 				'q': title
 			}
 		});
-		if (artist === undefined) {
-			for (const hit of response.data.response.hits) {
-				console.log(hit.result.featured_artists);
-			}
-		}
 		return response.data.response.hits[0].result.url;
 	} catch (error) {
 		console.error(error);
@@ -30,12 +26,42 @@ const getURL = async (title, artist) => {
 };
 
 
+const scrapeLyrics = async (url) => {
+	const browser = await chromium.puppeteer.launch({
+		args: [...chromium.args, "--hide-scrollbars", "--disable-web-security"],
+		defaultViewport: chromium.defaultViewport,
+		executablePath: await chromium.executablePath,
+		headless: true,
+		ignoreHTTPSErrors: true,
+	});
+
+	const page = await browser.newPage();
+	await page.setRequestInterception(true);
+	page.on('request', (req) => {
+		if (req.resourceType() == 'font' || req.resourceType() == 'image') {
+			req.abort();
+		}
+		else {
+			req.continue();
+		}
+	});
+	await page.goto(url);
+
+	const selector = `div[data-lyrics-container="true"]`
+	await page.waitForSelector(selector);
+	const lyrics = await page.$$eval(selector, arrayOfDivs => arrayOfDivs.map(div => div.innerText));
+
+	await browser.close();
+
+	return lyrics;
+};
+
+
 const app = express();
 app.use(cors());
 
 app.get("/", async (req, res) => {
-	if (req.query.title) res.send(await getURL(req.query.title))
-	else res.send(null);
+	res.send(await scrapeLyrics(await getURL('дао')));
 });
 
 const port = process.env.PORT || 5000;
