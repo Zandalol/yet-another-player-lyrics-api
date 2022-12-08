b9587997ff09f4714026a2b8a8f138765a4513a2
Zandalol <Leptus365@gmail.com>
2022-12-09 01:00:16 +0700

    6

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 require('dotenv').config()
-const axios = require('axios');
 const express = require('express');
+const cheerio = require('cheerio');
+const axios = require('axios');
 const cors = require('cors');
+const p = require('phin');
 
 
-const getURL = async (title, artist) => {
+const callAPI = async (title) => {
 	try {
 		const response = await axios({
 			method: 'GET',
@@ -18,23 +20,80 @@ const getURL = async (title, artist) => {
 				'q': title
 			}
 		});
-		// if (artist === undefined) {
-		// 	for (const hit of response.data.response.hits) {
-		// 		console.log(hit.result.featured_artists);
-		// 	}
-		// };
-		return response.data.response.hits[0].result.url;
+		return response.data.response.hits[0].result;
 	} catch (error) {
 		console.error(error);
 	};
 };
 
 
+const getInfo = (obj) => {
+	console.log(obj);
+	return new Object({
+		artist: {
+			name: obj.primary_artist.name,
+			image_url: obj.primary_artist.image_url,
+			header_image_url: obj.primary_artist.header_image_url
+		},
+		featured_artists: obj.featured_artists,
+		title: obj.title,
+		title_with_featured: obj.title_with_featured,
+		song_art_image_url: obj.song_art_image_url,
+		song_art_image_thumbnail_url: obj.song_art_image_thumbnail_url,
+		release_date_components: obj.release_date_components,
+		release_date_for_display: obj.release_date_for_display,
+		language: obj.language,
+	})
+};
+
+
+const scrapeLyrics = async (url) => {
+	const res = await p(url)
+	try {
+		const fullHTML = res.body
+
+		const $ = cheerio.load(fullHTML)
+		let lyrics = $('div.lyrics').text()
+
+		/* genius.org serves two DOMs for its lyrics pages, the below
+		   scrapes the second style (that does not contain a lyrics div) */
+
+		if (!lyrics) {
+			$('[class^=Lyrics__Container]').each((i, el) => {
+				const html = $(el).html()
+				const lined = html.replace(/<br\s*[\/]?>/gi, "\n")
+				const stripped = lined.replace(/<[^>]+>/ig, '')
+				const trimmed = stripped.trim()
+				// console.log(stripped)
+				lyrics += trimmed
+			})
+		}
+		if (!lyrics || fullHTML.includes('Lyrics for this song have yet to be')) {
+			console.log('Failed to capture lyrics or none present')
+			if (fullHTML.includes('Burrr!'))
+				console.log('could not find url ', url)
+			return null
+		}
+
+		return lyrics
+	}
+	catch (e) {
+		console.log(e)
+		return null
+	}
+};
+
+
 const app = express();
 app.use(cors());
 
-app.get("/", async (req, res) => {
-	if (req.query.title) res.send(await getURL(req.query.title))
+app.get("/lyrics", async (req, res) => {
+	if (req.query.title) res.send(await scrapeLyrics(await callAPI(req.query.title)));
+	else res.send(null);
+});
+
+app.get("/info", async (req, res) => {
+	if (req.query.title) res.send(getInfo(await callAPI(req.query.title)))
 	else res.send(null);
 });
 
